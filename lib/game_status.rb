f698642c3ec041710e838759f5824dc600c6539f
Roberto Garza <robertg4567@hotmail.com>
2018-07-11 15:44:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,6 +23,8 @@ def won?(board)
 
     if board[win_combination[0]] == "X" &&  board[win_combination[1]]  == "X" &&  board[win_combination[2]]  == "X"
       return win_combination
+    elsif board[win_combination[0]] == "O" &&  board[win_combination[1]]  == "O" &&  board[win_combination[2]]  == "O"
+      return win_combination
     else
       false
     end
