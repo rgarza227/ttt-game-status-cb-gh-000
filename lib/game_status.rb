a18b99d6a5237834fa8fc4cadf5eae946b41abdf
Roberto Garza <robertg4567@hotmail.com>
2018-07-11 15:35:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
 
-    
+
     if board[win_index_1] == "X" &&  board[win_index_2]  == "X" &&  board[win_index_3]  == "X"
       win_combination
     else
