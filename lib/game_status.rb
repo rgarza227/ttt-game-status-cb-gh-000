8c2d821eaec764c3b0e9c6371bf760210f535779
Roberto Garza <robertg4567@hotmail.com>
2018-07-11 15:40:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,21 +9,19 @@ WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
+  #Horizontal Wins #COLUMNS
   [0,3,6],
   [1,4,7],
   [2,5,8],
+  #Diagonal Wins
   [0,4,8],
   [2,4,6]
 ]
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
 
-
-    if board[win_index_1] == "X" &&  board[win_index_2]  == "X" &&  board[win_index_3]  == "X"
+    if board[win_combination[0]] == "X" &&  board[win_combination[1]]  == "X" &&  board[win_combination[2]]  == "X"
       return win_combination
     else
       false
