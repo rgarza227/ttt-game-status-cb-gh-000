1a74b666fc0401cbd04c9c2d82ff1773f50a64b6
Roberto Garza <robertg4567@hotmail.com>
2018-07-11 17:24:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,11 +58,5 @@ end
 
 def winner(board)
   if won?(board)
-    index = won?(board)
-    if board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
+    return board[won?(board)[0]]
 end
