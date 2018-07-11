0988015de1bb6cf37313686eca2c31ac869b3191
Roberto Garza <robertg4567@hotmail.com>
2018-07-11 15:39:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,15 +5,13 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  #Vertical Wins
+  #Vertical Wins #ROWS
   [0,1,2],
   [3,4,5],
   [6,7,8],
-  #Horizontal Wins
   [0,3,6],
   [1,4,7],
   [2,5,8],
-  #Diagonal Wins
   [0,4,8],
   [2,4,6]
 ]
